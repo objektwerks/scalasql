1623f8d3d0b337fc11a3aa8122c03a24f8492e82
objektwerks <objektwerks@runbox.com>
2025-06-14 10:53:35 -0400

    edited todo

--- src/main/scala/objektwerks/Todo.scala
+++ src/main/scala/objektwerks/Todo.scala
@@ -4,6 +4,6 @@ import scalasql.namedtuples.SimpleTable
 
 type Count = Int
 
-object Todo extends SimpleTable[Todo]
-
 final case class Todo(id: Int = 0, task: String) derives CanEqual
+
+object Todo extends SimpleTable[Todo]

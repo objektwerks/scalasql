8a05fa361c4a1d49c302682178abe2887ed8a6db
objektwerks <objektwerks@runbox.com>
2025-06-14 10:43:40 -0400

    added account

--- src/main/scala/objektwerks/Entity.scala
+++ src/main/scala/objektwerks/Entity.scala
@@ -12,6 +12,14 @@ object Entity:
 
   def nowPlusOneDay: String = LocalDate.now.plusDays(1).toString
 
+final case class Account(
+  id: Long = 0,
+  license: String = UUID.randomUUID.toString,
+  email: String = "",
+  pin: String = Pin.newInstance,
+  activated: String = Entity.now
+) extends Entity derives CanEqual
+
 final case class Participant(
   id: Long = 0,
   email: String,
